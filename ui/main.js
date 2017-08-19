17a921765914cee141442a2a499021f754766a5d
jaganselva <jaganselva@gmail.com>
2017-08-19 13:38:32 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,2 @@
 console.log('Loaded!');
 'https://sh-s7-live-s.legocdn.com/is/image/LEGOMKTG/missing-pieces'
-<img class="aligncenter" src="http://4.bp.blogspot.com/-ZOyTm3Ui1xk/WJ-sjSON6GI/AAAAAAAABgM/Lrx0IlsZdPUXAhaOxJsxGl_iGGIGTkr6gCK4B/s400/tamilgun.gif">
